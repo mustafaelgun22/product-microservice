62abe728b6dc7624ac4ec9051a0189523858f938
mustafa <elgunmustafa2@gmail.com>
2023-02-28 17:51:22 +0300

    response status added to exceptions

--- src/main/java/com/productmicroservice/productmicroservice/Exception/ProductBarcodeNotFound.java
+++ src/main/java/com/productmicroservice/productmicroservice/Exception/ProductBarcodeNotFound.java
@@ -1,5 +1,9 @@
 package com.productmicroservice.productmicroservice.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ProductBarcodeNotFound extends RuntimeException{
     public ProductBarcodeNotFound(String m) {
         super(m);
--- src/main/java/com/productmicroservice/productmicroservice/Exception/ProductNotFoundException.java
+++ src/main/java/com/productmicroservice/productmicroservice/Exception/ProductNotFoundException.java
@@ -1,5 +1,9 @@
 package com.productmicroservice.productmicroservice.Exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class ProductNotFoundException extends RuntimeException{
     public ProductNotFoundException(String m) {
          super(m);
